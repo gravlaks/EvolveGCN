03668330b9494ab6f6eb579d1bd3f96416352048
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2023-03-16 14:37:22 -0700

    num nodes

--- taskers_utils.py
+++ taskers_utils.py
@@ -35,8 +35,9 @@ def get_1_hot_deg_feats(adj,max_deg,num_nodes):
     #For now it'll just return a 2-hot vector
     new_vals = torch.ones(adj['idx'].size(0))
     new_adj = {'idx':adj['idx'], 'vals': new_vals}
-    degs_out, _ = get_degree_vects(new_adj,num_nodes)
     num_nodes = 6006
+
+    degs_out, _ = get_degree_vects(new_adj,num_nodes)
     degs_out = {'idx': torch.cat([torch.arange(num_nodes).view(-1,1),
                                   degs_out.view(-1,1)],dim=1),
                 'vals': torch.ones(num_nodes)}
