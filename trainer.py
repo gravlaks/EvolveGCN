2f1fb46a33af9b06c959527f05ca6785ebc5c771
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2023-03-16 13:41:13 -0700

    token

--- trainer.py
+++ trainer.py
@@ -153,7 +153,7 @@ class Trainer():
         def prepare_sample(self,sample):
                 sample = u.Namespace(sample)
                 for i,adj in enumerate(sample.hist_adj_list):
-                        adj = u.sparse_prepare_tensor(adj,torch_size = [self.num_nodes]).to_dense()
+                        adj = u.sparse_prepare_tensor(adj,torch_size = [self.num_nodes]).to_dense().nonzero().t().contiguous() 
                         sample.hist_adj_list[i] = adj.to(self.args.device)
 
                         nodes = self.tasker.prepare_node_feats(sample.hist_ndFeats_list[i])
