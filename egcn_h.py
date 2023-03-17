e3b2f783749d9a284e992de05d7bf8555d7d10c6
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2023-03-17 16:02:51 -0700

    print

--- egcn_h.py
+++ egcn_h.py
@@ -136,12 +136,13 @@ class GRCU_GAT(torch.nn.Module):
             
             # mask = mask_list[t].to_dense()
             
-            print("node_embs", node_embs.shape)
             #first evolve the weights from the initial and use the new weights with the node_embs
             # GCN_weights = self.evolve_weights(GCN_weights,node_embs,mask_list[t])
             print("mask_list", mask_list[t])
             mask = mask_list[t].flatten()
             node_embs = node_embs_list[t].to_dense().t()
+            print("node_embs", node_embs.shape)
+
             input_GRU = torch.sum(torch.mul(torch.softmax(mask, dim=0), node_embs), axis=1)
             hidden_GRU = GCN_weights.flatten()
             print("input", input_GRU.shape)
